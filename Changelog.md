0e83319ee0f949dacf46600c8effd6eb91e8dbf1
Karol Galanciak <karol.galanciak@gmail.com>
2021-01-06 13:28:27 +0100

    release 0.2.0

--- Changelog.md
+++ Changelog.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 0.2.0
+- Introduce `persist_self_dropped_jobs` config option
+
 ## 0.1.0
 - Initial release
--- lib/sidekiq/robust/job/version.rb
+++ lib/sidekiq/robust/job/version.rb
@@ -1,7 +1,7 @@
 module Sidekiq
   module Robust
     module Job
-      VERSION = "0.1.0"
+      VERSION = "0.2.0"
     end
   end
 end
